4b718c40930199de2b5feb2fe7822c5212ea5d75
gene-yoo <gene.y.yoo@gmail.com>
2017-11-30 16:28:16 -0500

    Fix bug in initial JSON render for home page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      get '/home', to: 'users#index'
+      get '/home', to: 'users#index', as: 'home_path'
     end
   end
 end
