6d4bb1fba3d74766860698af980017fced6da643
rebeccanewborn <rjn58@cornell.edu>
2017-12-06 18:07:06 -0500

    article serializer timestamps

--- app/serializers/article_serializer.rb
+++ app/serializers/article_serializer.rb
@@ -1,3 +1,3 @@
 class ArticleSerializer < ActiveModel::Serializer
-  attributes :id, :title, :published_at, :description, :url, :image_url, :author, :user_id, :username
+  attributes :id, :title, :published_at, :description, :url, :image_url, :author, :user_id, :username, :created_at, :updated_at
 end
